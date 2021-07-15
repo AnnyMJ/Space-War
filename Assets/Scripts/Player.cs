a4889f4ae1d1c92d26b3725487edd12f90469a27
AnnyMJ <annymorontajimenez@gmail.com>
2021-07-15 16:41:20 -0400

    space war game progress so far

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This script defines which sprite the 'Player" uses and its health.
+/// </summary>
+
+public class Player : MonoBehaviour
+{
+    public GameObject destructionFX;
+
+    public static Player instance; 
+
+    private Rigidbody2D rb;
+    public float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (instance == null) 
+            instance = this;
+    }
+
+    //method for damage proceccing by 'Player'
+    public void GetDamage(int damage)   
+    {
+        Destruction();
+    }    
+
+    //'Player's' destruction procedure
+    void Destruction()
+    {
+        Instantiate(destructionFX, transform.position, Quaternion.identity); //generating destruction visual effect and destroying the 'Player' object
+        Destroy(gameObject);
+    }
+
+    void FixedUpdate() {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontalMove, verticalMove);
+        rb.velocity = movement * speed;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
